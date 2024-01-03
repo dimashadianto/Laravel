40df64cf6935c6b1f72ea55b99de749a34426be0
dimashadianto <dimashadianto460@gmail.com>
2024-01-03 23:57:17 +0700

    Create code for login and register

---
+++ app/Livewire/Auth/Register.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Register extends Component
+{
+    public $name, $email, $password, $password_confirmation;
+
+    public function render()
+    {
+        return view('livewire.auth.register')->extends('layouts.app')->section('content');
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => ['required'],
+            'email' => ['required','email','unique:users'],
+            'password' => ['required','confirmed'],
+        ];
+    }
+
+    public function registerUser() 
+    {
+        $this->validate();
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+        ]);
+        Auth::login($user, true);
+        
+        return redirect()->to(RouteServiceProvider::HOME);
+    }
+}
